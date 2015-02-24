64e198b7b3943bf7a50f9b4305831e24abc16c74
SvenniDal <svennidal@svennidal.com>
2015-02-24 22:49:12 +0000

    gulp task, concatinating and minifying now works perfectly

--- gulpfile.js
+++ gulpfile.js
@@ -1,12 +1,14 @@
 var gulp = require('gulp'),
 		uglify = require('gulp-uglify'),
 		concat = require('gulp-concat'),
-		notify = require('gulp-notify');
+		notify = require('gulp-notify'),
+		ngAnnotate = require('gulp-ng-annotate');
 
 gulp.task('js', function(){
 	return gulp.src(['./vendor/jquery/dist/jquery.min.js', './vendor/angular/angular.min.js', './vendor/angular-route/angular-route.min.js', './js/socket.io.min.js', './js/ChatClientModuleConfig.js', './js/socket-factory.js', './js/LoginController.js', './js/RoomsController.js', './js/RoomController.js', './js/contains.js']) 
+		.pipe(ngAnnotate())
 		.pipe(concat('app.js')) 
-		//.pipe(uglify()) 
+		.pipe(uglify()) 
 		.pipe(gulp.dest('build'))
 		.pipe(notify({ message: 'Finished minifying'}));
 });
